548c9e266399ec4904c4c669c1063f2dbcb8b961
Jason Anderson <Jason.Anderson@jasonsblog.place>
2022-03-15 19:54:45 -0500

    Minor touch-up

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -26,102 +26,102 @@ export (NodePath) var right_confetti_path: NodePath = @""
 onready var right_confetti: CPUParticles2D = get_node( right_confetti_path )
 
 func _ready() -> void:
-    # Pause the action until "start" has been pushed.
-    get_tree().set_pause( true )
-    # Assign the default colors to the "pause" menu colors for consistency.
-    player1_pause_color_picker.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 1 colors/ColorPickerButton".get_pick_color()
-    player2_pause_color_picker.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 2 colors/ColorPickerButton".get_pick_color()
-    # Also assign it to the confetti.
-    left_confetti.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 1 colors/ColorPickerButton".get_pick_color()
-    right_confetti.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 2 colors/ColorPickerButton".get_pick_color()
+	# Pause the action until "start" has been pushed.
+	get_tree().set_pause( true )
+	# Assign the default colors to the "pause" menu colors for consistency.
+	player1_pause_color_picker.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 1 colors/ColorPickerButton".get_pick_color()
+	player2_pause_color_picker.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 2 colors/ColorPickerButton".get_pick_color()
+	# Also assign it to the confetti.
+	left_confetti.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 1 colors/ColorPickerButton".get_pick_color()
+	right_confetti.color = $"Title/MarginContainer/VBoxContainer/Player colors Hbox/Player 2 colors/ColorPickerButton".get_pick_color()
 
 func _notification( what: int ) -> void:
-    # Check for when the game is sent to the background on Android.
-    match what:
-        NOTIFICATION_APP_PAUSED:
-            # Pause the action.
-            get_tree().paused = true
-            # Show the "pause" dialog.
-            $Pause_container.visible = true
+	# Check for when the game is sent to the background on Android.
+	match what:
+		NOTIFICATION_APP_PAUSED:
+			# Pause the action.
+			get_tree().paused = true
+			# Show the "pause" dialog.
+			$Pause_container.visible = true
 
 # A player scored a point.
 func _on_Out_of_bounds_left_scored() -> void:
-    set_right_score( 1 )
-    $Right_score.text = str( get_right_score() ) + '/3'
+	set_right_score( 1 )
+	$Right_score.text = str( get_right_score() ) + '/3'
 
 func _on_Out_of_bounds_right_scored() -> void:
-    set_left_score( 1 )
-    $Left_score.text = str( get_left_score() ) + "/3"
+	set_left_score( 1 )
+	$Left_score.text = str( get_left_score() ) + "/3"
 
 # Stuff for player 1's score.
 func set_left_score( point: int ) -> void:
-    left_player_score += point
-    $Left_score.text = str( get_left_score() )
+	left_player_score += point
+	$Left_score.text = str( get_left_score() )
 
 func get_left_score() -> int:
-    if left_player_score >= 3:
-        win( "Player 1" )
-    return left_player_score
+	if left_player_score >= 3:
+		win( "Player 1" )
+	return left_player_score
 
 # Stuff for player 2's score.
 func set_right_score( point: int ) -> void:
-    right_player_score += point
-    $Right_score.text = str( get_right_score() )
+	right_player_score += point
+	$Right_score.text = str( get_right_score() )
 
 func get_right_score() -> int:
-    if right_player_score >= 3:
-        win( "Player 2" )
-    return right_player_score
+	if right_player_score >= 3:
+		win( "Player 2" )
+	return right_player_score
 
 # A player won; display a message showing they won.
 func win( who: String ) -> void:
-    get_tree().set_pause( true )
-    $Winning_sound.play()
-    # Stop the confetti from emitting.
-    left_confetti.emitting = false
-    right_confetti.emitting = false
-    winner_container_label.text = who + " is the winner!"
-    winner_container.show()
+	get_tree().set_pause( true )
+	$Winning_sound.play()
+	# Stop the confetti from emitting.
+	left_confetti.emitting = false
+	right_confetti.emitting = false
+	winner_container_label.text = who + " is the winner!"
+	winner_container.show()
 
 # Reset all the things.
 func _on_Restart_button_pressed() -> void:
-    left_player_score = 0
-    right_player_score = 0
-    $Left_score.text = "0/3"
-    $Right_score.text = "0/3"
-    $Left.reset()
-    $Right.reset()
-    $Mr_puck.reset()
-    get_tree().set_pause( false )
-    # Hide the window.
-    winner_container.visible = not winner_container.is_visible()
+	left_player_score = 0
+	right_player_score = 0
+	$Left_score.text = "0/3"
+	$Right_score.text = "0/3"
+	$Left.reset()
+	$Right.reset()
+	$Mr_puck.reset()
+	get_tree().set_pause( false )
+	# Hide the window.
+	winner_container.visible = not winner_container.is_visible()
 
 # Unpause and start the game.
 func _on_Start_game_pressed() -> void:
-    $Title.visible = not $Title.is_visible()
-    get_tree().set_pause( false )
+	$Title.visible = not $Title.is_visible()
+	get_tree().set_pause( false )
 
 # Resume the action.
 func _on_Resume_button_pressed() -> void:
-    get_tree().paused = not get_tree().is_paused()
-    $Pause_container.hide()
+	get_tree().paused = not get_tree().is_paused()
+	$Pause_container.hide()
 
 # Pause the action.
 func _on_Pause_button_pressed() -> void:
-    get_tree().paused = true
-    $Pause_container.show()
+	get_tree().paused = true
+	$Pause_container.show()
 
 # Assign the color picker in the pause menu when the player selects it at the start.
 func _on_player1_color_changed( color: Color ) -> void:
-    # Close the virtual keyboard since it keeps popping up when the color picker comes on-screen.
-    # Answer found here:https://godotengine.org/qa/123555/how-can-i-make-the-android-keyboard-close?show=123617#a123617
-    OS.hide_virtual_keyboard()
-    player1_pause_color_picker.color = color
-    left_confetti.color = color
-    left_confetti.modulate = color
+	# Close the virtual keyboard since it keeps popping up when the color picker comes on-screen.
+	# Answer found here:https://godotengine.org/qa/123555/how-can-i-make-the-android-keyboard-close?show=123617#a123617
+	OS.hide_virtual_keyboard()
+	player1_pause_color_picker.color = color
+	left_confetti.color = color
+	left_confetti.modulate = color
 
 func _on_player2_color_changed( color: Color ) -> void:
-    OS.hide_virtual_keyboard()
-    player2_pause_color_picker.color = color
-    right_confetti.color = color
-    right_confetti.modulate = color
+	OS.hide_virtual_keyboard()
+	player2_pause_color_picker.color = color
+	right_confetti.color = color
+	right_confetti.modulate = color
